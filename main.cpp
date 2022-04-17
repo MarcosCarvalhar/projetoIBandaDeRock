6dc16a4d305db78398cb8ed796bb616bd67150e3
RelampaguMv <103449415+RelampaguMv@users.noreply.github.com>
2022-04-17 19:44:12 -0300

    Update main.cpp
    Cometário do codigo

--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@
 
 using namespace std;
 
+// Criação da estrura de bandas. 
 struct banda
 {
 	string nome, estilo;
 	int id, ano, numFamosas;
 };
 
+
+// Escopo das funções utilizadas.
 void listar(banda *rock, int tam);
 banda *incluir(banda *rock, int& tam);
 banda *redimencionar(banda *rock, int& tam);
@@ -17,31 +20,31 @@ void excluir(banda *rock, int exl, int opc2);
 void ordenar(banda *vetor, int menor, int maior, int tam, int opc2);
 void junta(banda *vetor, int menor, int maior, int metade, int tam, int opc2);
 
+// Programa principal.
 int main()
 {
+	// Declaração de variáveis.
 	int opc, tam = 0, exl, alt, opc2;
 	banda *rock = new banda[5];
-
+	// Exibição do menu e suas funcionalidades.
 	while(opc != 6)
 	{
 		cout <<    "\t MENU"  << endl;
-		cout << "1 - Incluir" << endl;
-		cout << "2 - Exluir"  << endl;
-		cout << "3 - Listar"  << endl;
-		cout << "4 - Alterar" << endl;
-		cout << "5 - Ordenar" << endl;
-		cout << "6 - Sair"    << endl;
+		cout << "1 - Incluir" << endl;	// Inclui um novo elemento no vetor.
+		cout << "2 - Exluir"  << endl;	// Exclui um elemento ou campo de uma posição do vetor.
+		cout << "3 - Listar"  << endl;	// Lista os elementos do vetor.
+		cout << "4 - Alterar" << endl;	// Altera um elemento ou uma posição inteira do vetor.
+		cout << "5 - Ordenar" << endl;	// Ordena o vetor com base na escolha do usuário.
+		cout << "6 - Sair"    << endl;	// Sair do menu e do programa.
 		cout << "Digite a opcao desejada : " << endl;	
-
 		cin  >> opc;
-
 		switch(opc)
 		{
 			case 1 : 
-            	rock = incluir(rock, tam);
+            	rock = incluir(rock, tam);	// Chamada da função para preencher uma posição do vetor, retornando o vetor rock.
       		break;
-
 			case 2 : 
+				// Exclui um elemento ou campo específico fornecido pelo usuário do vetor.
             	cout << "Digite a posicao do vetor que deseja excluir : " << endl;
 				cin  >> exl;
 				cout << "1 - Nome | 2 - Estilo | 3 - Ano de criacao | 4 - Numero de musicas famosas | 5 - Excluir a posicao inteira" << endl;
@@ -49,12 +52,11 @@ int main()
 				cin  >> opc2;
   		 		excluir(rock,exl,opc2);
       		break;
-
 			case 3 : 
-            	listar(rock, tam);
+            	listar(rock, tam);	// Chamada da função para listar e apresentar o vetor.
       		break;
-
 			case 4 : 
+				// Altera um elemento ou campo específico fornecido pelo usuário do vetor.
            		cout << "Digite a posicao do vetor que deseja alterar : " << endl;
 				cin  >> alt;
 				cout << "1 - Nome | 2 - Estilo | 3 - Ano de criacao | 4 - Numero de musicas famosas | 5 - Todos os dados " << endl;
@@ -62,79 +64,75 @@ int main()
 				cin  >> opc2;
 				alterar(rock,alt,opc2);
       		break;
-
 			case 5 :
+				// Ordena o vetor com base nos parâmetros do usuário.
 				cout << "1 - ID | 3 - Ano de criacao | 5 - Numero de musicas famosas"<< endl;
             	cout << "Digite o tipo que deseja ordenar : " << endl;
-				cin  >> opc2;
-				
-				ordenar(rock, 0, tam-1, tam, opc2);
-				
+				cin  >> opc2;				
+				ordenar(rock, 0, tam-1, tam, opc2);		
       		break;
 		}
 	}
 	return 0;
 }
 
+// Função redimencionar, recebe um vetor e seu tamanho e aumenta sua posição em uma, e retorna o vetor.
 banda* redimencionar(banda *rock, int& tam)
 {
 	banda *aux = new banda[tam+1];
 	memcpy(aux, rock, sizeof(banda)*tam);
-	
 	delete []rock;
 	rock = NULL;
-
 	return aux;
 }
 
+// Função incluir, recebe um vetor e sua posição e a preenche.
 banda* incluir(banda *rock, int& tam)
 {
+	// Se o vetor for maior ou igual a 5, chama a função redimencionar para aumentar o tamanho do vetor.
 	if(tam >= 5)
 	{	
 		rock = redimencionar(rock,tam);
 	}
-
+	// Preenchimento do campo id da estruta banda, através de um contador.
 	rock[tam].id = tam + 1;
-
+	// Preenchimento das outras variáveis da estrura banda.
 	cout << "Digite o nome :" << endl;
 	cin.ignore();
 	getline(cin,rock[tam].nome);
-
 	cout << "Digite o estilo musical :" << endl;
 	cin  >> rock[tam].estilo;
-
 	cout << "Digite o ano de criacao :" << endl;
 	cin  >> rock[tam].ano;
-
 	cout << "Digite o numero de musicas famosas:" << endl;
 	cin  >> rock[tam].numFamosas;
-
 	tam++;
-
 	return rock;
 }
 
-
+// Função excluir, exclui uma posição inteira do vetor ou campo especifico à marcando com uma flag, -1 para os campos int, e um espaço em branco para os campos string.
+// Recebe um vetor de rock e a posição que será excluída.
 void excluir(banda *rock, int exl, int opc2)
 {	
 	switch(opc2)
 	{
+		// Excluí o campo nome de uma posição específica do vetor.
 		case 1 : 
 			rock[exl].nome = " ";
 		break;
-		
+		// Excluí o campo estilo musical de uma posição específica do vetor.
 		case 2 : 
 			rock[exl].estilo = " ";
 		break;
-		
+		// Excluí o campo ano de criação de uma posição específica do vetor.
 		case 3 : 
 			rock[exl].ano = -1;
 		break;
-		
+		// Excluí o campo numero de músicas famosas de uma posição específica do vetor.
 		case 4 : 
 			rock[exl].numFamosas = -1;
 		break;
-
+		// Excluí todos os campos de uma posição específica do vetor.
 		case 5 : 
 			rock[exl].id         = -1;
 			rock[exl].nome       = " ";
@@ -145,10 +143,12 @@ void excluir(banda *rock, int exl, int opc2)
 	}
 }
 
+// Recebe um vetor e seu tamanho e lista as posições preenchidas, exibindo suas informações, através de um laço
 void listar(banda *rock, int tam)
 {
 	for(int j = 0; j < tam; j++)
 	{
+		// Caso o campo ID esteja marcado pela fla, -1, essa posição do vetor não será exibida
 		if(rock[j].id != -1)
 		{
 			cout << "\t"                           << rock[j].nome       << endl;
@@ -156,13 +156,16 @@ void listar(banda *rock, int tam)
 			cout << "Estilo da banda : "           << rock[j].estilo     << endl;
 			cout << "Ano de criacao : "            << rock[j].ano        << endl;
 			cout << "Numero de musicas famosas : " << rock[j].numFamosas << endl;
+			cout << "Posicao no vetor : " <<  rock[j].id - 1 << endl;
 			cout << endl;
 		}
 	}
 }
 
+// Função alterar, recebe um vetor, a posição que deseja alterar e qual campo
 void alterar(banda *rock, int alt, int opc2)
 {
+	// Caso o campo ID esteja marcado pela fla, -1, essa posição não poderá ser alterada, pois foi excluída
 	if(rock[alt].id == -1)
 	{
 		cout << "Essa posicao foi excluida, portanto nao pode ser alterada!" << endl;
@@ -171,38 +174,36 @@ void alterar(banda *rock, int alt, int opc2)
 	{
 		switch(opc2)
 		{
+			// Altera o campo nome de uma posição específica do vetor.
 			case 1 : 
 				cout << "Digite o nome :" << endl;
 				cin.ignore();
 				getline(cin,rock[alt].nome);
 			break;
-
+			// Altera o campo estilo musical de uma posição específica do vetor.
 			case 2 : 
 				cout << "Digite o estilo musical :" << endl;
 				cin  >> rock[alt].estilo;
 			break;
-
+			// Altera o campo ano de criação de uma posição específica do vetor.
 			case 3 : 
 				cout << "Digite o ano de criacao :" << endl;
 				cin  >> rock[alt].ano;
 			break;
-
+			// Altera o campo numero de músicas famosas de uma posição específica do vetor.
 			case 4 : 
 				cout << "Digite o numero de musicas famosas:" << endl;
 				cin  >> rock[alt].numFamosas;
 			break;
-
+			// Altera todos os campo de uma posição específica do vetor.
 			case 5 :
 				cout << "Digite o nome :" << endl;
 				cin.ignore();
-				getline(cin,rock[alt].nome);
-				
+				getline(cin,rock[alt].nome);				
 				cout << "Digite o estilo musical :" << endl;
 				cin  >> rock[alt].estilo;
-
 				cout << "Digite o ano de criacao :" << endl;
 				cin  >> rock[alt].ano;
-
 				cout << "Digite o numero de musicas famosas:" << endl;
 				cin  >> rock[alt].numFamosas;
 			break;
@@ -210,11 +211,13 @@ void alterar(banda *rock, int alt, int opc2)
 	}
 }
 
+// Função para ordenar o vetor com base na opção escolhida pelo usuário
+// Recebe o vetor de rock e seu tamanho, o menor e o maior campo e a opção com o campo na qual o vetor será será ordenado
 void ordenar(banda *vetor, int menor, int maior, int tam, int opc2)
 {
 	int metade;
-
-	if(menor < maior){
+	if(menor < maior)
+	{
 		metade = (menor + maior) / 2;
 		ordenar(vetor, menor, metade, tam, opc2);
 		ordenar(vetor, metade+1, maior, tam, opc2);
@@ -228,9 +231,7 @@ void junta(banda *vetor, int menor, int maior, int metade, int tam, int opc2)
 	int aux1 = menor;
 	int aux2 = menor;
 	int aux3 = metade+1;
-
 	int pri_Posicao, ult_Posicao;
-
 	while (aux1 <= metade and aux3 <= maior)
 	{
 		switch (opc2)
@@ -239,18 +240,15 @@ void junta(banda *vetor, int menor, int maior, int metade, int tam, int opc2)
 			pri_Posicao = vetor[aux1].id;
 			ult_Posicao = vetor[aux3].id;
 			break;
-
 			case 3:
 			pri_Posicao = vetor[aux1].ano;
 			ult_Posicao = vetor[aux3].ano;
 			break;
-
 			case 5:
 			pri_Posicao = vetor[aux1].numFamosas;
 			ult_Posicao = vetor[aux3].numFamosas;
 			break;
 		}
-
 		if (pri_Posicao < ult_Posicao) 
 		{
 			v[aux2] = vetor[aux1];
@@ -281,4 +279,3 @@ void junta(banda *vetor, int menor, int maior, int metade, int tam, int opc2)
         vetor[i] = v[i];
     }	
 }
-
